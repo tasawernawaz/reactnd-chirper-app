79aebb7c13f388b0a59fee7f9a1b62208231d062
tasawernawaz <tasawer.nawaz@arbisoft.com>
2021-01-16 21:41:15 +0500

    fix: minor fix in logger middleware

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -2,7 +2,7 @@ const logger = (store) => (next) => (action) => {
     console.group(action)
         console.log("The action is", action)
         const result = next(action)
-        console.log("The result of the action is", result)
+        console.log("The result of the action is", store.getState())
     console.groupEnd()
 
     return result
