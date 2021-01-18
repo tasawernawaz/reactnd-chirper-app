712ec93bb19f7262c4e55b95cddd8bded7ec256d
tasawernawaz <tasawer.nawaz@arbisoft.com>
2021-01-18 19:45:05 +0500

    feat: loading bar implemented

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,16 +2,19 @@ import { getInitialData } from '../utils/api'
 import { receiveTweets } from '../actions/tweets'
 import { receiveUsers } from '../actions/users'
 import { authUser } from '../actions/authUser'
+import { showLoading, hideLoading} from 'react-redux-loading'
 
 
 const AUTH_USER_ID = "tylermcginnis"
 
 export function handleInitialData() {
     return (dispatch) => {
+        dispatch(showLoading())
         return getInitialData().then(({tweets, users}) => {
             dispatch(receiveTweets(tweets))
             dispatch(receiveUsers(users))
             dispatch(authUser(AUTH_USER_ID))
+            dispatch(hideLoading())
         })
 
     }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux'
 import users from '../reducers/users'
 import tweets from '../reducers/tweets'
 import authUser from '../reducers/authUser'
+import { loadingBarReducer } from 'react-redux-loading'
 
 
 export default combineReducers({
     authUser,
     tweets,
-    users
+    users,
+    loadingBar: loadingBarReducer
 })
