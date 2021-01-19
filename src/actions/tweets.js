d6368c8a8f2fe9be2448aac705182a2d41eb0b32
tasawernawaz <tasawer.nawaz@arbisoft.com>
2021-01-19 18:13:26 +0500

    feat: implement toggle like tweet

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,8 +1,35 @@
+import { saveLikeToggle } from '../utils/api'
 export const RECEIVE_TWEETS = "RECEIVE_TWEETS"
+export const LIKE_TWEET = "LIKE_TWEET"
+
+
 
 export function receiveTweets(tweets) {
     return {
         type: RECEIVE_TWEETS,
         tweets
+    }
+}
+
+
+export function likeTweet(tweetId, hasLiked, authUser) {
+    return {
+        type: LIKE_TWEET,
+        tweetId: tweetId,
+        authUser: authUser,
+        hasLiked: hasLiked
+    }
+}
+
+
+export function handleLikeTweet(id, hasLiked, authUser) {
+    return (dispatch) => {
+        dispatch(likeTweet(id, hasLiked, authUser))
+        return saveLikeToggle({id, hasLiked, authUser})
+        .catch((e) => {
+            console.warn('Error in handleToggleTweet: ', e)
+            dispatch(likeTweet(id, hasLiked, authUser))
+            alert('There was an error liking the tweet. Try again.')
+        })
     }
 }
--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,4 +1,5 @@
-import { RECEIVE_TWEETS } from '../actions/tweets'
+import { authUser } from '../actions/authUser'
+import { LIKE_TWEET, RECEIVE_TWEETS } from '../actions/tweets'
 
 export default function users (state={}, action) {
     // TODO: check the spread operator
@@ -7,6 +8,16 @@ export default function users (state={}, action) {
             return {
                 ...state,
                 ...action.tweets
+            }
+        case LIKE_TWEET:
+            return {
+                ...state,
+                [action.tweetId]: {
+                    ...state[action.tweetId],
+                    likes: action.hasLiked === true ?
+                    state[action.tweetId].likes.filter(uid => uid !== action.authUser)
+                    :state[action.tweetId].likes.concat(action.authUser)
+                }
             }
         default:
             return state
